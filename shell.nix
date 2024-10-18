89d9298da96f299279cbad9ed29b06c758630ad6
Olivier Norture <olivier.norture@ultra.io>
2024-10-18 13:17:45 +0000

    add pojo generation test

--- shell.nix
+++ shell.nix
@@ -6,5 +6,9 @@ mkShell {
   hardeningDisable = [ "all" ]; # https://github.com/go-delve/delve/issues/3085
   buildInputs = [
     pkgs.go
+
+    # Use to test avro to POJO generation
+    pkgs.wget
+    pkgs.temurin-bin
   ];
 }
